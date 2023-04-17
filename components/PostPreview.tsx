449f179315c3e980ee3c01f764ff3ff2b8d69aef
Manish Kumar <mkumar@acvauctions.com>
2023-04-17 15:10:25 -0400

    Initial commit

---
+++ components/PostPreview.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from "next/link"
+import { PostMetadata } from './postMetadata'
+
+const PostPreview = (props: PostMetadata) => {
+  return (
+    <div className='border border-slate-200 p-4 rounded-md shadow-md bg-white'>
+    <Link href={`/posts/${props.slug}`}>
+        <h2 className='font-bold text-violet-600 hover:underline'>{props.title}</h2>
+    </Link>  
+    <p className='text-sm text-slate-400 mb-4'>{props.date}</p> 
+    <p className='text-slate-700'>{props.subtitle}</p>
+  </div>
+  )
+}
+
+export default PostPreview
---
+++ components/getPostMetadata.ts
@@ -0,0 +1,25 @@
+import { PostMetadata } from "./postMetadata"
+import matter from "gray-matter"
+import fs from "fs"
+
+const getPostMetadata = (): PostMetadata[] => {
+    const folder = "posts/"
+    const files = fs.readdirSync(folder)
+    const markdownPosts = files.filter((file) => file.endsWith(".md"))
+  
+    // Get gray-matter data from each post
+    const posts = markdownPosts.map((fileName) => {
+      const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8")
+      const matterResult = matter(fileContents)
+      return {
+        title: matterResult.data.title,
+        date: matterResult.data.date,
+        subtitle: matterResult.data.subtitle,
+        slug: fileName.replace(".md", "")
+      }
+    })
+  
+    return posts
+  }
+
+  export default getPostMetadata;
